a2a788d461964244d3b151c546e86ec46ed45163
Martin Puza <m.puza@zetes.at>
2016-10-10 13:06:52 +0200

    Revert "added focus on touch point"
    This reverts commit baf77fbdbb194798b290962215d2417e0e1153c2.

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import CameraManager
 
-
 class ViewController: UIViewController {
     
     // MARK: - Constants
@@ -31,7 +30,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         cameraManager.showAccessPermissionPopupAutomatically = false
         
         askForPermissionsButton.isHidden = true
@@ -80,12 +79,6 @@ class ViewController: UIViewController {
     }
 
     // MARK: - @IBActions
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            cameraManager.focus(atPoint : touch.location(in: cameraView))
-        }
-    }
 
     @IBAction func changeFlashMode(_ sender: UIButton)
     {
