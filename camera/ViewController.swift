de3d221832dbcfd5d34779dbdfdf2bdf289f9231
Andrea Mazzini <andrea.mazzini@gmail.com>
2018-10-04 12:17:57 +0200

    Fix demo project

--- camera/ViewController.swift
+++ camera/ViewController.swift
@@ -38,6 +38,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//      cameraManager.shouldRespondToOrientationChanges = false
         cameraManager.shouldEnableExposure = true
         
         cameraManager.shouldFlipFrontCameraImage = false
@@ -111,7 +112,7 @@ class ViewController: UIViewController {
         cameraManager.showErrorBlock = { [weak self] (erTitle: String, erMessage: String) -> Void in
         
             let alertController = UIAlertController(title: erTitle, message: erMessage, preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (alertAction) -> Void in  }))
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (alertAction) -> Void in  }))
             
             self?.present(alertController, animated: true, completion: nil)
         }
@@ -151,7 +152,7 @@ class ViewController: UIViewController {
             })
         case .videoWithMic, .videoOnly:
             cameraButton.isSelected = !cameraButton.isSelected
-            cameraButton.setTitle("", for: UIControlState.selected)
+            cameraButton.setTitle("", for: UIControl.State.selected)
     
             cameraButton.backgroundColor = cameraButton.isSelected ? redColor : lightBlue
             if sender.isSelected {
